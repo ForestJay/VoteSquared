ec53754f56f58365e0f20ea8f36ab988c9569041
forhan <FHandford@carbonite.com>
2014-08-14 20:14:34 -0400

    Added show for a politician and added listing of all politicians for issue #1.

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -3,5 +3,24 @@ class PoliticiansController < ApplicationController
   end
 
   def create
+    @politician = Politician.new(politician_params)
+    
+     @politician.save
+     redirect_to @politician
+  end
+  
+  def show
+    @politician = Politician.find(params[:id])
+  end
+  
+  def index
+    @politicians = Politician.all
+  end
+  
+  # Private members
+  private
+  
+  def politician_params
+    params.require(:politician).permit(:first_name, :last_name, :country, :state, :current_office)
   end
 end
